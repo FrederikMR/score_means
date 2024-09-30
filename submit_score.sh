e6615015bada9e2510555b738658ede0da11bb2e
Frederik-DTU <s164222@student.dtu.dk>
2024-09-30 18:57:28 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J nSphere2_stism
+#BSUB -J HypParaboloid2_stism
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 4:00
@@ -18,20 +18,21 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold nSphere \
+    --manifold HyperbolicParaboloid \
     --dim 2 \
     --loss_type ism \
     --load_model 0 \
     --T_sample 0 \
     --t0 0.1 \
+    --sigma 100.0 \
     --gamma 1.0 \
-    --train_net s1 \
+    --train_net st \
     --T 1.0 \
     --lr_rate 0.001 \
     --epochs 50000 \
     --warmup_epochs 1000 \
-    --x0_samples 10 \
-    --xt_samples 32 \
+    --x0_samples 1 \
+    --xt_samples 1024 \
     --t_samples 100 \
     --dt_steps 100 \
     --save_step 100 \
