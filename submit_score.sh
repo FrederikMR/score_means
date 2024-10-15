c066257257d356174fe4862960849e47296dff5d
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2024-10-15 21:53:06 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Cylinder_s1ism
+#BSUB -J nSphere_s1ism
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 4:00
@@ -18,22 +18,22 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold Cylinder \
+    --manifold nSphere \
     --dim 2 \
     --loss_type ism \
     --load_model 0 \
     --T_sample 0 \
     --t0 0.1 \
-    --sigma 1.0 \
     --gamma 1.0 \
+    --sigma 100.0 \
     --train_net st \
+    --eps 0.01 \
     --T 1.0 \
     --lr_rate 0.001 \
     --epochs 50000 \
     --warmup_epochs 1000 \
     --x0_samples 1 \
-    --xt_samples 1024 \
-    --t_samples 100 \
+    --xt_samples 512 \
     --dt_steps 100 \
     --save_step 100 \
     --seed 2712
